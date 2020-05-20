2e0fd5ff13b9f3c1cb5b898066ed902c25fe55a2
kole-swapnil <swapnilkole7500@gmail.com>
2020-05-20 22:12:57 +0530

    Some warnings resolved

--- wallet.sol
+++ wallet.sol
@@ -19,7 +19,7 @@ contract wallet{
     event sentmoney(address indexed add1,uint amt1);
     event recmoney(address indexed add2,uint amt2);
     modifier onlyOwner(){
-        require(msg.sender == owner);
+        require(msg.sender == owner,"You are not the owner");
         _;
     }
     modifier whilenotpaused(){
@@ -29,32 +29,26 @@ contract wallet{
     function change(bool ch)public onlyOwner{
         pause = ch;
     }
-    
     function sendmoney()public payable whilenotpaused {
         Balance_record[msg.sender].totbal += msg.value;
-        Balance_record[msg.sender].numpay+=1;
+        Balance_record[msg.sender].numpay += 1;
         Payment memory pay = Payment(msg.value,now);
         Balance_record[msg.sender].payments[Balance_record[msg.sender].numpay] = pay;
         emit sentmoney(msg.sender,msg.value);
     }
-    
     function getbal()public view whilenotpaused returns(uint){
         return Balance_record[msg.sender].totbal ;
     }
-    
     function convert(uint amtinwei)public pure returns(uint){
         return amtinwei/1 ether;
     }
     function withdraw(uint _amt)public whilenotpaused{
         require(Balance_record[msg.sender].totbal >= _amt,"not enough funds");
-        
         Balance_record[msg.sender].totbal -= _amt;
         msg.sender.transfer(_amt);
         emit recmoney(msg.sender,_amt);
-        
     }
     function destroy(address payable ender)public onlyOwner{
         selfdestruct(ender);
     }
-    
 }
